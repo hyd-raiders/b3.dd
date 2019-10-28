ebe33e81da9f23a34a2400a2a55f1329139fa6b4
apporoad <apporoadina@163.com>
2019-10-28 18:33:28 +0800

    add

--- subs/sink/build.sbt
+++ subs/sink/build.sbt
@@ -5,5 +5,7 @@ version := "0.1"
 scalaVersion := "2.11.12"
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.2.0"
+
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.2.0" % "provided"
 // https://mvnrepository.com/artifact/org.apache.spark/spark-streaming-kafka-0-10
 libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-10" % "2.2.0"
--- subs/sink/src/main/scala/com/xdo/sink/simple.scala
+++ subs/sink/src/main/scala/com/xdo/sink/simple.scala
@@ -1,25 +1,62 @@
 package com.xdo.sink
 
-import org.apache.spark._
-import org.apache.spark.streaming._
-import org.apache.spark.streaming.StreamingContext._
-import org.apache.spark.streaming.kafka._
 
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka010._
 import java.util.HashMap
+
+import org.apache.spark._
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 
 
 object simple {
   def main(args: Array[String]) {
-    val ssc = new StreamingContext(sc, Seconds(5))
+    val conf = new SparkConf().setMaster("local[2]").setAppName("SparkStreamingTest")
+    val sc = new SparkContext(conf)
+    val ssc = new StreamingContext(sc,Seconds(5))
+    //nc -l -p 7777
+    val lines = ssc.socketTextStream("localhost",7777)
 
-    val topicMap = Map("test" -> 1)
+    val wordsDstream= lines.flatMap(_.split(" "))
+    //6、每一个单词计为1
+    val wordAndOneDstream = wordsDstream.map((_,1))
+    //7、相同单词出现的次数累加
+    val result= wordAndOneDstream.reduceByKey(_+_)
+    //8、打印结果
+    result.print()
 
-    //read data
-    val lines = KafkaUtils.createStream(ssc,"hadoop:2181","testWordCountGroup",topicMap).map(_._2)
-    val words = lines.flatMap(_.split(" "))
-    val wordCounts = words.map( x => (x, 1)).reduceByKey(_ + _)
-    wordCounts.print()
     ssc.start()
+
+
+    //该方法为一直阻塞在这里，等待程序结束
     ssc.awaitTermination()
+
+
+
+
+
+
+
+//    val conf = new SparkConf().setAppName("singleSink").setMaster("local[n]")
+//    val ssc= new StreamingContext(conf,Seconds(10))
+//
+//    //kafka parames
+//    val kafkaParams = Map[String, Object](
+//      "bootstrap.servers" -> "192.168.10.158:19092",
+//      "key.deserializer" -> classOf[StringDeserializer],
+//      "value.deserializer" -> classOf[StringDeserializer],
+//      "group.id" -> "use_a_separate_group_id_for_each_stream",
+//      "auto.offset.reset" -> "latest",
+//      "enable.auto.commit" -> (false: java.lang.Boolean)
+//    )
+//    val topics = Array("bury")
+//    val stream = KafkaUtils.createDirectStream(
+//      ssc,
+//      PreferConsistent,
+//      Subscribe[String, String](topics, kafkaParams)
+//    )
+//    stream.start()
   }
 }
